43ed93b825b8db9aef2814d636c690d93cc85330
hvelarde <hector.velarde@gmail.com>
2013-05-13 12:17:28 -0300

    promote package to beta

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
 
 import os
 
@@ -18,7 +19,7 @@ setup(
     description=description,
     long_description=long_description,
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: 4.2",
